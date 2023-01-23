7e6c3c502cdd7cc247b90a97feabd9d492bde68b
antonhopkins12@gmail.com <antonhopkins12@gmail.com>
2023-01-23 15:04:23 +0300

    изменения

--- GitApplication/Program.cs
+++ GitApplication/Program.cs
@@ -7,6 +7,7 @@ namespace GitApplication
         static void Main(string[] args)
         {
             MatrixA(2, 4);
+           
         }
         static void MatrixA(int w, int h)
         {
@@ -25,22 +26,6 @@ namespace GitApplication
             Console.ForegroundColor = ConsoleColor.Black;
             
         }
-        public static void MatrixB(int w,int h)
-        {
-            Console.ForegroundColor = ConsoleColor.DarkBlue;
-            Random r = new Random();
-            int[,] array = new int[w,h];
-           for(int i = 0; i < w; i++)
-            {
-                for (int j = 0; j < h; j++)
-                {
-                    array[i, j] = r.Next(0, 2);
-                    Console.Write($"{array[i,j]} ");
-                }
-                Console.WriteLine();
-            }
-            Console.ForegroundColor = ConsoleColor.White;
-
-        }
+        
     }
 }
