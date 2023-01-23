a350ffe3b15310e651bff7e2b5ce9c0398afc22e
Пётр Черкасов <petr.a.cherkasov@outlook.com>
2023-01-23 15:04:03 +0300

    без функции А

--- GitApplication/Program.cs
+++ GitApplication/Program.cs
@@ -6,24 +6,6 @@ namespace GitApplication
     {
         static void Main(string[] args)
         {
-            MatrixA(2, 4);
-        }
-        static void MatrixA(int w, int h)
-        {
-            Console.ForegroundColor = ConsoleColor.DarkGreen;
-            Random random = new Random();
-            int[,] array = new int[w, h];
-            for (int i = 0; i < w; i++)
-            {
-                for(int j = 0; j < h; j++)
-                {
-                    array[i, j] = random.Next(0, 2);
-                    Console.Write($"{array[i, j]} ");
-                }
-                Console.WriteLine();
-            }
-            Console.ForegroundColor = ConsoleColor.Black;
-            
         }
         public static void MatrixB(int w,int h)
         {
